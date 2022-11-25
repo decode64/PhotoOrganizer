8759e7fb5d58fa11f4ac3e28efef4585383e45c5
Adriano Luz <84450848+decode64@users.noreply.github.com>
2022-11-25 01:58:02 +0000

    Refactor solution

--- lib/photo_organizer.rb
+++ lib/photo_organizer.rb
@@ -1,18 +1,21 @@
-Photo = Struct.new(:filename, :location, :date)
+Photo = Struct.new(:filename, :location, :date, :index)
 
 def solution(input)
   photos = input.lines(chomp: true).map do |line|
     Photo.new(*line.split(",").map(&:strip))
   end
 
-  grouped_photos = photos.group_by(&:location)
-                         .transform_values { |photos| photos.sort_by(&:date) }
+  photos.group_by(&:location).each_value { |group| assign_indices(group) }
 
   photos.map do |photo|
-    leading_zeros = grouped_photos[photo.location].size.digits.size
-    index = grouped_photos[photo.location].index(photo) + 1
-    index = "%0#{leading_zeros}d" % index
-
-    "#{photo.location}#{index}#{File.extname(photo.filename)}"
+    "#{photo.location}#{photo.index}#{File.extname(photo.filename)}"
   end.join("\n")
+end
+
+def assign_indices(photo_group)
+  digits = photo_group.size.digits.size
+  photo_group.sort_by(&:date)
+             .each.with_index(1) do |photo, index|
+               photo.index = "%0#{digits}d" % index
+             end
 end
